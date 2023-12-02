d903e3290a265b9fad36b99225fd239b8e7acabf
Sagor <sagorsamadder51@gmail.com>
2023-12-02 14:09:24 +0600

    if allready login go to home page when open app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:s_medi/firebase_options.dart';
 import 'package:s_medi/general/consts/consts.dart';
 import 'app/auth/view/login_page.dart';
+import 'app/home/view/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +12,31 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var isLogin = false;
+  var auth = FirebaseAuth.instance;
+  chekIfLogin() async {
+    auth.authStateChanges().listen((User? user) {
+      if (user != null && mounted) {
+        setState(() {
+          isLogin = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    chekIfLogin();
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
@@ -25,7 +49,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff4B2EAD)),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: isLogin ? const Home() : const LoginView(),
     );
   }
 }
