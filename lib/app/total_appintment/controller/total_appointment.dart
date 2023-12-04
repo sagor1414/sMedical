b6ef643139f064a237f8905b743084dd16655ef0
Sagor <sagorsamadder51@gmail.com>
2023-12-04 21:30:49 +0600

    fixing bugs

--- lib/app/total_appintment/controller/total_appointment.dart
+++ lib/app/total_appintment/controller/total_appointment.dart
@@ -3,6 +3,12 @@ import 'package:s_medi/general/consts/consts.dart';
 class TotalAppointmentcontroller extends GetxController {
   var docName = ''.obs;
   Future<QuerySnapshot<Map<String, dynamic>>> getAppointments() async {
-    return FirebaseFirestore.instance.collection('appointments').get();
+    return FirebaseFirestore.instance
+        .collection('appointments')
+        .where(
+          'appBy',
+          isEqualTo: FirebaseAuth.instance.currentUser?.uid,
+        )
+        .get();
   }
 }
