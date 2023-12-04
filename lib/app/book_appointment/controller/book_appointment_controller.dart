3d9006879c0483998cb0f2916b39b78789ce949a
Sagor <sagorsamadder51@gmail.com>
2023-12-04 20:01:44 +0600

    validate appoinment data

--- lib/app/book_appointment/controller/book_appointment_controller.dart
+++ lib/app/book_appointment/controller/book_appointment_controller.dart
@@ -7,21 +7,39 @@ class AppointmentController extends GetxController {
   var appNameController = TextEditingController();
   var appMobileController = TextEditingController();
   var appMessageController = TextEditingController();
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
   bookAppointment(String docId, context) async {
-    isLoading(true);
-    var store = FirebaseFirestore.instance.collection('appointments').doc();
-    await store.set({
-      'appBy': FirebaseAuth.instance.currentUser?.uid,
-      'appDay': appDayController.text,
-      'appTime': appTimeController.text,
-      'appName': appNameController.text,
-      'appMobile': appMobileController.text,
-      'appMsg': appMessageController.text,
-      'appWith': docId,
-    });
-    isLoading(false);
-    VxToast.show(context, msg: "Appointment is booked sucessfully");
-    Get.back();
+    if (formkey.currentState!.validate()) {
+      try {
+        isLoading(true);
+        var store = FirebaseFirestore.instance.collection('appointments').doc();
+        await store.set({
+          'appBy': FirebaseAuth.instance.currentUser?.uid,
+          'appDay': appDayController.text,
+          'appTime': appTimeController.text,
+          'appName': appNameController.text,
+          'appMobile': appMobileController.text,
+          'appMsg': appMessageController.text,
+          'appWith': docId,
+        });
+        isLoading(false);
+        VxToast.show(context, msg: "Appointment is booked sucessfully");
+        Get.back();
+      } catch (e) {
+        VxToast.show(context, msg: "$e");
+      }
+    }
+  }
+
+  String? validdata(value) {
+    if (value!.isEmpty) {
+      return 'please fill this';
+    }
+    RegExp emailRefExp = RegExp(r'^.{3,}$');
+    if (!emailRefExp.hasMatch(value)) {
+      return 'Enter valid data';
+    }
+    return null;
   }
 }
--- lib/app/book_appointment/view/appointment_view.dart
+++ lib/app/book_appointment/view/appointment_view.dart
@@ -22,57 +22,64 @@ class BookAppointmentView extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              "Select Appointment date"
-                  .text
-                  .size(AppFontSize.size16)
-                  .semiBold
-                  .make(),
-              CoustomTextField(
-                textcontroller: controller.appDayController,
-                hint: "Select date",
-                icon: const Icon(Icons.calendar_month_outlined),
-              ),
-              10.heightBox,
-              "Select Appointment Time"
-                  .text
-                  .size(AppFontSize.size16)
-                  .semiBold
-                  .make(),
-              CoustomTextField(
-                textcontroller: controller.appTimeController,
-                hint: "Select time",
-                icon: const Icon(Icons.watch_later),
-              ),
-              10.heightBox,
-              "patient's name".text.size(AppFontSize.size16).semiBold.make(),
-              CoustomTextField(
-                textcontroller: controller.appNameController,
-                hint: "patient's full name",
-                icon: const Icon(Icons.person),
-              ),
-              10.heightBox,
-              "Mobile Number".text.size(AppFontSize.size16).semiBold.make(),
-              CoustomTextField(
-                textcontroller: controller.appMobileController,
-                hint: "Enter patent mobile number",
-                icon: const Icon(Icons.call),
-              ),
-              10.heightBox,
-              "Your problem".text.size(AppFontSize.size16).semiBold.make(),
-              TextFormField(
-                controller: controller.appMessageController,
-                maxLines: 3,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.note_add),
-                  hintText: "write your problem in short",
-                  hintStyle: TextStyle(),
-                  border: OutlineInputBorder(borderSide: BorderSide()),
+          child: Form(
+            key: controller.formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Select Appointment date"
+                    .text
+                    .size(AppFontSize.size16)
+                    .semiBold
+                    .make(),
+                CoustomTextField(
+                  validator: controller.validdata,
+                  textcontroller: controller.appDayController,
+                  hint: "Select date",
+                  icon: const Icon(Icons.calendar_month_outlined),
                 ),
-              )
-            ],
+                10.heightBox,
+                "Select Appointment Time"
+                    .text
+                    .size(AppFontSize.size16)
+                    .semiBold
+                    .make(),
+                CoustomTextField(
+                  validator: controller.validdata,
+                  textcontroller: controller.appTimeController,
+                  hint: "Select time",
+                  icon: const Icon(Icons.watch_later),
+                ),
+                10.heightBox,
+                "patient's name".text.size(AppFontSize.size16).semiBold.make(),
+                CoustomTextField(
+                  validator: controller.validdata,
+                  textcontroller: controller.appNameController,
+                  hint: "patient's full name",
+                  icon: const Icon(Icons.person),
+                ),
+                10.heightBox,
+                "Mobile Number".text.size(AppFontSize.size16).semiBold.make(),
+                CoustomTextField(
+                  validator: controller.validdata,
+                  textcontroller: controller.appMobileController,
+                  hint: "Enter patent mobile number",
+                  icon: const Icon(Icons.call),
+                ),
+                10.heightBox,
+                "Your problem".text.size(AppFontSize.size16).semiBold.make(),
+                TextFormField(
+                  controller: controller.appMessageController,
+                  maxLines: 3,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.note_add),
+                    hintText: "write your problem in short",
+                    hintStyle: TextStyle(),
+                    border: OutlineInputBorder(borderSide: BorderSide()),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
