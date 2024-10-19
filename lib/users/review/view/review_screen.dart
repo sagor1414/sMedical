af78b72d07ed87302316fac59b526eee07bc7d0c
sagor <85604284+sagor1414@users.noreply.github.com>
2024-10-19 17:41:05 +0600

    Update review_screen.dart

--- lib/users/review/view/review_screen.dart
+++ lib/users/review/view/review_screen.dart
@@ -17,103 +17,105 @@ class ReviewPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Behavior Selection
-            const Text(
-              'How was the behavior?',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Column(
-              children: <Widget>[
-                Obx(() => ListTile(
-                      title: const Text('Good'),
-                      leading: Radio(
-                        value: 'Good',
-                        groupValue: reviewController.selectedBehavior.value,
-                        onChanged: (value) {
-                          reviewController.updateBehavior(value!);
-                        },
-                      ),
-                    )),
-                Obx(() => ListTile(
-                      title: const Text('Average'),
-                      leading: Radio(
-                        value: 'Average',
-                        groupValue: reviewController.selectedBehavior.value,
-                        onChanged: (value) {
-                          reviewController.updateBehavior(value!);
-                        },
-                      ),
-                    )),
-                Obx(() => ListTile(
-                      title: const Text('Bad'),
-                      leading: Radio(
-                        value: 'Bad',
-                        groupValue: reviewController.selectedBehavior.value,
-                        onChanged: (value) {
-                          reviewController.updateBehavior(value!);
-                        },
-                      ),
-                    )),
-              ],
-            ),
-            const SizedBox(height: 20),
-            // Comment Section
-            const Text(
-              'Additional Comments (Optional)',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              controller: reviewController.commentController,
-              maxLines: 4,
-              decoration: const InputDecoration(
-                hintText: 'Enter your comments here...',
-                border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Behavior Selection
+              const Text(
+                'How was the behavior?',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 20),
-            // Rating Section with Stars
-            const Text(
-              'Give a Rating',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: List.generate(
-                  5, (index) => reviewController.buildStar(index + 1)),
-            ),
-            const SizedBox(height: 20),
-            // Submit Button
-            Center(
-              child: SizedBox(
-                width: context.screenWidth * .5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.primeryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+              Column(
+                children: <Widget>[
+                  Obx(() => ListTile(
+                        title: const Text('Good'),
+                        leading: Radio(
+                          value: 'Good',
+                          groupValue: reviewController.selectedBehavior.value,
+                          onChanged: (value) {
+                            reviewController.updateBehavior(value!);
+                          },
+                        ),
+                      )),
+                  Obx(() => ListTile(
+                        title: const Text('Average'),
+                        leading: Radio(
+                          value: 'Average',
+                          groupValue: reviewController.selectedBehavior.value,
+                          onChanged: (value) {
+                            reviewController.updateBehavior(value!);
+                          },
+                        ),
+                      )),
+                  Obx(() => ListTile(
+                        title: const Text('Bad'),
+                        leading: Radio(
+                          value: 'Bad',
+                          groupValue: reviewController.selectedBehavior.value,
+                          onChanged: (value) {
+                            reviewController.updateBehavior(value!);
+                          },
+                        ),
+                      )),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // Comment Section
+              const Text(
+                'Additional Comments (Optional)',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                controller: reviewController.commentController,
+                maxLines: 4,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your comments here...',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Rating Section with Stars
+              const Text(
+                'Give a Rating',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                children: List.generate(
+                    5, (index) => reviewController.buildStar(index + 1)),
+              ),
+              const SizedBox(height: 20),
+              // Submit Button
+              Center(
+                child: SizedBox(
+                  width: context.screenWidth * .5,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primeryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    reviewController.submitReview();
-                  },
-                  child: Obx(
-                    () => reviewController.loading.value
-                        ? Center(
-                            child: CircularProgressIndicator(
-                              color: AppColors.whiteColor,
+                    onPressed: () {
+                      reviewController.submitReview();
+                    },
+                    child: Obx(
+                      () => reviewController.loading.value
+                          ? Center(
+                              child: CircularProgressIndicator(
+                                color: AppColors.whiteColor,
+                              ),
+                            )
+                          : Text(
+                              'Submit Review',
+                              style: TextStyle(color: AppColors.whiteColor),
                             ),
-                          )
-                        : Text(
-                            'Submit Review',
-                            style: TextStyle(color: AppColors.whiteColor),
-                          ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
